bffac64ac4d075bd839ff4982ee67ce1eae8f653
Gilnior <gilnior123@gmail.com>
2021-04-23 15:44:23 -0300

    main is working

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# a ideia é pegar os nomes e os cursos e relacionar-los.
-
-# get names and courses
-
-# get names and grades
-
-# first: build a csv file with the course and the minimum grade used to pass
-
 # %%
 import pandas as pd
+
 # %%
 courseDF = pd.read_json('jsons/nomecurso.json')
-courseDF.head()
+courseDF = courseDF.rename(columns={'Id': 'IDs'})
+
 # %%
 gradeDF = pd.read_json('jsons/nomenota.json')
 gradeDF.head()
+
+# %%
+passed = courseDF['IDs']
+condition = gradeDF['IDs'].isin(passed)
+passedgrades = gradeDF.loc[condition]
+
+# %%
+passedgrades['Courses'] = courseDF['Course']
+passedgrades.head()
