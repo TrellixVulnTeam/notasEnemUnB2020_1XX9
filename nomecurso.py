7d14eab1b2863b9527fd9c8988adb6e26227cf70
Gilnior <gilnior123@gmail.com>
2021-04-29 07:34:00 -0300

    trying with re

--- nomecurso.py
+++ nomecurso.py
@@ -1,6 +1,7 @@
 # %%
 import PyPDF2
 import json
+import re
 
 nomecursoPDF = open('PDFs/nomecurso.PDF', 'rb')
 nomecurso = PyPDF2.PdfFileReader(nomecursoPDF)
@@ -16,7 +17,33 @@ def rawtext(pdf):
 def text():
     data = rawtext(nomecurso)
     start = data.find(".")
-    return data[start+400:-559].strip()  # return just the required data
+    # return just the required data
+    return data[start+400:-559].strip().replace('\n', '')
+
+
+def candidates(debug=False):
+    x = text()
+    columns = ['IDs', 'Courses']
+    candidates = {}
+
+    idp = re.compile(r'(\d){8}')
+    cpc = r'\d\s[A-ZÍíÃãÓóÇÔôÕõÊêÉéÂâÁáÉÚú\s()a-z]+\s/+'
+    coursep = re.compile(cpc)
+    patterns = [idp, coursep]
+
+    for i in range(len(columns)):
+        matches = patterns[i].finditer(x)
+        lista = []
+        for match in matches:
+            value = match.group()
+            if i == 0:
+                lista.append(value[:])
+            elif i == 1:
+                lista.append(value[2:])
+        if debug:
+            print(len(lista), end=', ',)
+        candidates[columns[i]] = lista
+    return candidates
 
 
 def splittext():
@@ -54,6 +81,7 @@ def idEcourses(debug=False):
     return di  # It needs len(c) == len(n)
 
 
+# %%
 m = idEcourses()
 j = json.dumps(m)
 
--- nomenota.py
+++ nomenota.py
@@ -1,6 +1,7 @@
 # %%
 import PyPDF2
 import json
+import re
 
 nomenotaPDF = open("PDFs/nomenota.PDF", "rb")
 nomenota = PyPDF2.PdfFileReader(nomenotaPDF)
@@ -8,100 +9,44 @@ nomenota = PyPDF2.PdfFileReader(nomenotaPDF)
 
 def rawtext(pdf):
     txt = ''
-    for page in range(pdf.numPages):
+    for page in range(40, pdf.numPages):
         txt += pdf.getPage(page).extractText()
     return txt.strip()
 
 
 def text():
-    data = rawtext(nomenota)
+    data = rawtext(nomenota).replace('\n', '')
     start = data.find("DIURNO")
     end = data.find("4 das")
     return data[start:end].strip()
 
 
-def rawcandidates():
-    data = text().replace(' \n-\n,', '').replace('\n-', '').replace('\n', '')
-    candidates0 = data.split("/")
-    candidates = candidates0[:-5]
-    lastcandl = candidates0[-5].split('.')
-    lastcand = lastcandl[0]+lastcandl[1]
-    candidates.append(lastcand)
+def candidates(debug=False):
+    x = text()
+    columns = ['IDs', 'Grades']
+    candidates = {}
+
+    idp = re.compile(r'\d{8},')
+    gradep = re.compile(r'\s\d\d\d\.\d\d')
+    patterns = [idp, gradep]
+
+    for i in range(len(columns)):
+        matches = patterns[i].finditer(x)
+        lista = []
+        for match in matches:
+            value = match.group()
+            if i == 0:
+                lista.append(value[:8])
+            elif i == 1:
+                lista.append(value[:])
+        if debug:
+            print(len(lista), end=', ')
+        candidates[columns[i]] = lista
     return candidates
 
 
-def candidates():
-    data = rawcandidates()
-    lista = []
-    for candidate in data:
-        if not candidate[0].isdecimal():
-            try:
-                candidatel = candidate.split(")")
-                realcandidate = candidatel[-1].strip()
-                lista.append(realcandidate)
-            except Exception:
-                # except Exception lets me use control+c to stop the code
-                candidatel = candidate.split("*")
-                realcandidate = candidatel[-1].strip()
-                lista.append(realcandidate)
-        else:
-            lista.append(candidate.strip())
-    return lista
-
-
-def todic(debug=False):
-    pessoas = candidates()
-    IDs = []
-    grades = []
-    for candidate in pessoas:
-        wds = candidate.split(',')
-        IDs.append(fixID(wds[0].strip()))
-        for wd in wds:
-            pl = wd.strip()
-            if len(pl) >= 6 and wds[0] != wd and wd != wds[1] and wd != "(BACHARELADO":
-                grades.append(fixgrade(pl))
-
-    IDs = byenones(IDs)
-    grades = byenones(grades)
-
-    dic = {'IDs': IDs, 'Grades': grades}
-
-    if debug:
-        print(len(IDs), len(grades))
-
-    return dic
-
-
-def fixgrade(grade):
-    if len(grade) > 6:
-        x = grade.split()
-        for i in x:
-            if len(i) == 6 and not i.isalpha():
-                return i
-    elif len(grade) == 6:
-        return grade
-    else:
-        return None
-
-
-def fixID(ID):
-    if len(ID) > 8 and not ID.isdecimal():
-        x = ID.split()
-        if x[-1].isdecimal():
-            return x[-1].strip()
-    elif not ID.isdecimal():
-        return None
-    else:
-        return ID
-
-
-def byenones(nlist):
-    while None in nlist:
-        nlist.remove(None)
-    return(nlist)
-
-
-m = todic()
+# %%
+m = candidates()
 j = json.dumps(m)
 
 with open('jsons/nomenota.json', 'w') as f:
